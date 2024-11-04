b3d77836acb1522384faf16241fc269906e90a11
Carles Capellas <carles@peekvision.org>
2024-11-04 20:43:11 +0100

    Sample express server

---
+++ projects/express-server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  // Simplified authorization for demonstration purposes
+  if (!req.headers.authorization) {
+    return res.status(401).send('Unauthorized')
+  }
+
+  next();
+});
+
+app.get('/get-endpoint', (_req, res, _next) => { res.send('Hello world'); });
+
+app.post('/post-endpoint', (req, res, _next) => { res.send(`Hello world. ${JSON.stringify(req.body)}`); });
+
+app.listen(process.env.PORT ||3000, () => {
+  console.log('Server up & running');
+});
